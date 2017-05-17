61d16ad51b5c2bf25b8d39c3c47b67b768d63219
Francisco Romano <s171872@ad.aie.edu>
2017-05-17 12:59:06 +1000

    Basic Event structure

---
+++ Reversi/source/main.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+}
---
+++ Reversi/source/main.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "Window.h"
+#include "Application.h"
