c8e09eb82a46b7f90e8866d8762dcebd4dea9060
Francisco Romano <s171872@ad.aie.edu>
2017-05-24 10:21:27 +1000

    Testing, VSCOMUNITY 2015 required

--- vsprojects/Tutorials/source/main.cpp
+++ vsprojects/Tutorials/source/main.cpp
@@ -1,10 +1,13 @@
 #include <ctime>
 #include <random>
 #include <iostream>
+#include <Windows.h>
 #include <CCON\ccon.h>
 
 void main()
 {
+
+
     ccon::cconInit();
     ccon::cconSize(40, 40);
     ccon::cconViewport(0, 0, 40, 40);
