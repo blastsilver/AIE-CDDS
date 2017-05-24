064b2e809ba26e745e94aff65b9d0cee68015037
Francisco Romano <s171872@ad.aie.edu>
2017-05-24 12:53:44 +1000

    Linked list working (problem with PopBack/PushBack)

--- vsprojects/Tutorials/source/main.cpp
+++ vsprojects/Tutorials/source/main.cpp
@@ -4,9 +4,27 @@
 #include <Windows.h>
 #include <CCON\ccon.h>
 
+#include "List.h"
+
 void main()
 {
+    List<int> list;
 
+    for (int i = 0; i < 10; i++)
+    {
+        list.pushBack(i);
+        //list.pushFront(i);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        list.popBack();
+        list.popFront();
+    }
+
+    for (List<int>::Node * i = list.first(); i != list.end(); i = i->next)
+    {
+        std::cout << i->value << ",";
+    }
 
     ccon::cconInit();
     ccon::cconSize(40, 40);
