3f2dc1e25f6debbee5d4525af6afa60d7915394d
Francisco Romano <s171872@ad.aie.edu>
2017-06-05 16:56:52 +1000

    Red and Black Tree

--- vsprojects/Tutorials/source/main.cpp
+++ vsprojects/Tutorials/source/main.cpp
@@ -4,134 +4,20 @@
 #include <Windows.h>
 #include <CCON\ccon.h>
 #include "CGraphics.h"
-#include "Tutorials.h"
-
-#define SIZE 81
-
-struct vec2 { float x, y; };
-struct vec4 { float x, y, z, w; };
-
-float worldToScreen(int n)
-{
-    float halfSize = (float(SIZE) - 1.0) / 2.0f;
-    return (float(n) / halfSize) - 1.0f;
-}
-void fillTopFlatTriangle(CGraphics * graphics, vec2 & v1, vec2 & v2, vec2 & v3)
-{
-    float invslope1 = (v3.x - v1.x) / (v3.y - v1.y);
-    float invslope2 = (v3.x - v2.x) / (v3.y - v2.y);
-
-    float curx1 = v3.x;
-    float curx2 = v3.x;
-
-    for (int scanlineY = v3.y; scanlineY > v1.y; scanlineY--)
-    {
-        graphics->colour(worldToScreen(curx1) / 2.0f + 1.0f, worldToScreen(scanlineY) / 2.0f + 1.0f, 0);
-        graphics->vertex(worldToScreen(curx1), -worldToScreen(scanlineY));
-
-        graphics->colour(worldToScreen(curx2) / 2.0f + 1.0f, worldToScreen(scanlineY) / 2.0f + 1.0f, 0);
-        graphics->vertex(worldToScreen(curx2), -worldToScreen(scanlineY));
-        curx1 -= invslope1;
-        curx2 -= invslope2;
-    }
-}
-void fillBottomFlatTriangle(CGraphics * graphics, vec2 & v1, vec2 & v2, vec2 & v3)
-{
-    float invslope1 = (v2.x - v1.x) / (v2.y - v1.y);
-    float invslope2 = (v3.x - v1.x) / (v3.y - v1.y);
-
-    float curx1 = v1.x;
-    float curx2 = v1.x;
-
-    for (int scanlineY = v1.y; scanlineY <= v2.y; scanlineY++)
-    {
-        graphics->colour(worldToScreen(curx1) / 2.0f + 1.0f, worldToScreen(scanlineY) / 2.0f + 1.0f, 0);
-        graphics->vertex(worldToScreen(curx1), -worldToScreen(scanlineY));
-
-        graphics->colour(worldToScreen(curx2) / 2.0f + 1.0f, worldToScreen(scanlineY) / 2.0f + 1.0f, 0);
-        graphics->vertex(worldToScreen(curx2), -worldToScreen(scanlineY));
-        curx1 += invslope1;
-        curx2 += invslope2;
-    }
-}
-void swapVertices(vec2 * v1, vec2 * v2)
-{
-    vec2 temp = { v1->x, v1->y };
-    v1->x = v2->x;
-    v1->y = v2->y;
-    v2->x = temp.x;
-    v2->y = temp.y;
-}
-void sortVerticesAscendingByY(vec2 * v1, vec2 * v2, vec2 * v3)
-{
-    if (v1->y < v2->y) swapVertices(v1, v2);
-    if (v1->y < v3->y) swapVertices(v1, v3);
-    if (v2->y < v3->y) swapVertices(v2, v3);
-    if (v3->y < v1->y) swapVertices(v3, v1);
-}
-void drawTriangle(CGraphics * graphics, vec2 & v1, vec2 & v2, vec2 & v3)
-{
-    /* at first sort the three vertices by y-coordinate ascending so v1 is the topmost vertice */
-    sortVerticesAscendingByY(&v1, &v2, &v3);
-
-    /* here we know that v1.y <= v2.y <= v3.y */
-    /* check for trivial case of bottom-flat triangle */
-    if (v2.y == v3.y)
-    {
-        fillBottomFlatTriangle(graphics, v1, v2, v3);
-    }
-    /* check for trivial case of top-flat triangle */
-    else if (v1.y == v2.y)
-    {
-        fillTopFlatTriangle(graphics, v1, v2, v3);
-    }
-    else
-    {
-        vec2 v4;
-        v4.y = v2.y;
-        v4.x = (v1.x + ((float)(v2.y - v1.y) / (float)(v3.y - v1.y)) * (v3.x - v1.x));
-        /* general case - split the triangle in a topflat and bottom-flat one */
-        fillBottomFlatTriangle(graphics, v1, v2, v4);
-        fillTopFlatTriangle(graphics, v2, v4, v3);
-    }
-}
-
+#include "TutorialsBK.h"
 
 void main()
 {
-    //ccon::cconInit();
-    //ccon::cconSize(SIZE, SIZE);
-    //ccon::cconViewport(0, 0, SIZE, SIZE);
-    //CGraphics graphics(SIZE, SIZE);
-    //graphics.mode(CGraphics::LINES);
-    //graphics.clear(CGraphics::DEPTH_BUFFER | CGraphics::COLOUR_BUFFER);
-    //drawTriangle(&graphics, vec2{ 10, 5 }, vec2{ 60, 0 }, vec2{ 0, 60 });
-    //graphics.draw();
-    //while (ccon::cconRunning());
-    //ccon::cconTerminate();
+    Tree<int> tree;
 
-    int size = 40;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3); // sort
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(6);
+    
 
-    LinkedList<int> list1;
-    LinkedList<int> list2;
+    std::cout << "GoodBye \n";
 
-    for (int i = 0; i < 5; i++)
-    {
-        list1.pushBack(i);
-        list1.pushFront(i);
-    }
-    list2 = list1;
-
-    std::cout << "--[ List 1 ]----------------------------\n - ";
-    for (Node<int> * i = list1.first(); i != list1.end(); i = i->next)
-        std::cout << i->value << (char)((i->next == nullptr) ? '\n' : ',');
-
-    std::cout << "--[ List 1 ]----------------------------\n - ";
-    for (Node<int> * i = list2.first(); i != list2.end(); i = i->next)
-        std::cout << i->value << (char)((i->next == nullptr) ? '\n' : ',');
-    std::cout << "----------------------------------------\n";
-
-    std::cout << "\n";
-
-    while (true);
 }
